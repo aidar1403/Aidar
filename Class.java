3fd4ee24953e4edb96ee5e6930aa875e8d748046
aidar1403 <aidar9976@icloud.com>
2026-02-04 12:04:52 +0600

    Commit

--- Class.java
+++ Class.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Scanner;
 //practice
 public class Class {
     public static void main(String[] args) {
@@ -135,8 +136,79 @@ int lastIndex = flowers.lastIndexOf("Rose");
                         new ArrayList<>(Arrays.asList(numbersArray));
 
                 System.out.println(numbersList);
-            }
+
+/*
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int n;
+        n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            int num = scanner.nextInt();
+            numbers.add(num);
         }
+        for (int num : numbers) {
+            System.out.print(num + " ");
+
+            System.out.println(numbers);
+        }
+        ArrayList<Integer> numbers2 = new ArrayList<>();
+        numbers2.add(7);
+        numbers2.add(9);
+        numbers2.add(1,8);
+        int last = numbers2.get(2);
+        System.out.println(last);
+        numbers2.set(0,5);
+        System.out.println(numbers2);
+        System.out.println(numbers2.size());
+
+        Arraylist();
+        simple();
+        newArraylist();
+    }
+    public static void Arraylist(){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(9);
+        numbers.add(10);
+        numbers.remove(1);
+        numbers.remove(Integer.valueOf(9));
+        if(numbers.contains(Integer.valueOf(8))) {
+            System.out.println("yes");
+        }
+        if(!numbers.isEmpty()){
+            System.out.println("Not empty");
+        }
+        System.out.println(numbers);
+        System.out.println(numbers.size());
+    }
+    public static void simple(){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(12);
+        numbers.add(3);
+        numbers.add(7);
+        numbers.add(1);
+        Collections.sort(numbers);
+        System.out.println(numbers);
+        Collections.sort(numbers,Collections.reverseOrder());
+        System.out.println(numbers);
+        Collections.shuffle(numbers);
+        System.out.println(numbers);
+        int minVal= Collections.min(numbers);
+        System.out.println(minVal);
+        int maxVal=Collections.max(numbers);
+        System.out.println(maxVal);
+    }
+    public static void newArraylist(){
+        ArrayList<Integer>zeros = new ArrayList<>(Collections.nCopies(10,0));
+        System.out.println(zeros);*/
+    }
+}
+
+
+
+
 
 
 
--- Main.java
+++ Main.java
@@ -1,74 +1,216 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
-        int n;
-        n = scanner.nextInt();
-        for (int i = 0; i < n; i++) {
-            int num = scanner.nextInt();
-            numbers.add(num);
-        }
-        for (int num : numbers) {
-            System.out.print(num + " ");
+    public static void main(String[] args){
+        //Practice 3
+        Scanner scanner = new Scanner (System.in);
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(4);
+        set.add(4);
+        set.add(5);
+        System.out.println(set);
+        set.clear();
 
-            System.out.println(numbers);
+        HashSet<String>names = new HashSet<>();
+        names.add("Karl");
+        names.add("Poul");
+        names.add("Jason");
+        System.out.println("enter a userName");
+        String name = scanner.nextLine();
+        if(names.contains(name)){
+            System.out.println("Allowed");
+        }else {
+            System.out.println("is not allowed");
         }
-        ArrayList<Integer> numbers2 = new ArrayList<>();
-        numbers2.add(7);
-        numbers2.add(9);
-        numbers2.add(1,8);
-        int last = numbers2.get(2);
-        System.out.println(last);
-        numbers2.set(0,5);
-        System.out.println(numbers2);
-        System.out.println(numbers2.size());
 
-Arraylist();
-simple();
-newArraylist();
+
+        HashSet<Integer>numbers = new HashSet<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        System.out.println(numbers.size());
+        numbers.clear();
+        System.out.println(numbers.isEmpty());
+
+        HashSet<String> names2 = new HashSet<>();
+        names2.add("Polo");
+        names2.add("Calvin");
+        names2.add("Klein");
+        names2.add("Michiel");
+        names2.add("Alex");
+        boolean removed = names2.remove("Polo");
+        System.out.println(removed);
+        System.out.println(names2);
+
+        HashSet<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        HashSet<Integer> set2 = new HashSet<>();
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+        set1.addAll(set2);
+        System.out.println(set1);
+
+        HashSet<Integer>setA = new HashSet<>();
+        setA.add(12);
+        setA.add(15);
+        setA.add(18);
+        setA.add(21);
+        setA.add(24);
+        HashSet<Integer>setB = new HashSet<>();
+        setB.add(12);
+        setB.add(14);
+        setB.add(16);
+        setB.add(18);
+        setB.add(20);
+        setA.removeAll(setB);
+        System.out.println(setA);
+
+        HashSet<Integer>setX = new HashSet<>();
+        setX.add(1);
+        setX.add(2);
+        setX.add(3);
+        setX.add(4);
+        setX.add(5);
+        HashSet<Integer>setY = new HashSet<>();
+        setY.add(2);
+        setY.add(3);
+        setY.add(4);
+        setX.retainAll(setY);
+        System.out.println(setX);
+
+        HashSet<String>SetA = new HashSet<>();
+        SetA.add("apple");
+        SetA.add("banana");
+        SetA.add("pea");
+        HashSet<String>SetB = new HashSet<>();
+        SetB.add("apple");
+        SetB.add("banana");
+        SetB.add("pea");
+        if (SetA.containsAll(SetB)){
+            System.out.println("All coincide!");
+        }
+        System.out.println("enter your sentence");
+        String sentence = scanner.nextLine();
+        String [] words = sentence.split(" ");
+        HashSet<String> uniqueWords = new HashSet<>();
+        for (String word:words){
+            uniqueWords.add(word);
+        }
+        System.out.println("unique words");
+        for (Iterator<String> iterator = uniqueWords.iterator(); iterator.hasNext(); ) {
+            String word = iterator.next();
+            System.out.println(word);
+
+            String sentence2 = "Apple apple APPLE Banana BANANA orange";
+            String[] words2 = sentence2.split(" ");
+
+            HashSet<String> uniqueWords2 = new HashSet<>();
+            for (String w : words2) {
+                uniqueWords.add(w.toLowerCase());
+            }
+            System.out.println(uniqueWords2);
+
+
+            ArrayDeque<Integer> queue = new ArrayDeque<>();
+            queue.offer(10);
+            queue.offer(20);
+            queue.offer(30);
+            while (!queue.isEmpty()) {
+                System.out.println("Peek: " + queue.peek());
+                System.out.println("Poll: " + queue.poll());
+            }
+
+            ArrayDeque<String> stack = new ArrayDeque<>();
+            stack.push("One");
+            stack.push("Two");
+            stack.push("Three");
+            while (!stack.isEmpty()) {
+                System.out.println(stack.pop());
+            }
+
+            ArrayDeque<Integer> deque = new ArrayDeque<>();
+            deque.addFirst(2);
+            deque.addLast(3);
+            deque.addFirst(1);
+            deque.addLast(4);
+            for (int n : deque) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+
+            ArrayDeque<Integer> deque2 = new ArrayDeque<>();
+            System.out.println(deque2.offerFirst(100));
+            System.out.println(deque2.offerLast(200));
+            System.out.println(deque2);
+
+            ArrayDeque<String> dq = new ArrayDeque<>();
+            dq.add("A");
+            dq.add("B");
+            dq.add("C");
+            System.out.println(dq.peek());
+            System.out.println(dq.peekFirst());
+            System.out.println(dq.peekLast());
+            dq.clear();
+            System.out.println(dq.peek());
+
+            ArrayDeque<String> dq2 = new ArrayDeque<>();
+            dq2.add("X");
+            dq2.add("Y");
+            dq2.add("Z");
+            while (!dq2.isEmpty()) {
+                System.out.println(dq2.pollFirst());
+            }
+
+
+            ArrayDeque<String> dq3 = new ArrayDeque<>(
+                    Arrays.asList("a", "b", "c", "b", "a")
+            );
+            dq3.removeFirstOccurrence("b");
+            dq3.removeLastOccurrence("a");
+            System.out.println(dq3);
+
+            ArrayDeque<Integer> dq4 = new ArrayDeque<>();
+            System.out.println(dq4.isEmpty());
+            dq4.add(1);
+            dq4.add(2);
+            System.out.println(dq4.size());
+            dq4.poll();
+            System.out.println(dq4.size());
+
+            ArrayDeque<String> dq5 = new ArrayDeque<>();
+            dq5.add("Hello");
+            dq5.add("World");
+            dq5.clear();
+            System.out.println(dq5.peek());
+            System.out.println(dq5.isEmpty());
+
+            String word2 = "level";
+            ArrayDeque<Character> chars = new ArrayDeque<>();
+            for (char c : word2.toCharArray()) {
+                chars.add(c);
+            }
+
+            boolean isPalindrome = true;
+            while (chars.size() > 1) {
+                if (chars.pollFirst() != chars.pollLast()) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            System.out.println(word + " -> " + isPalindrome);
+        }
     }
-    public static void Arraylist(){
-ArrayList<Integer> numbers = new ArrayList<>();
-numbers.add(1);
-numbers.add(7);
-numbers.add(8);
-numbers.add(9);
-numbers.add(10);
-numbers.remove(1);
-numbers.remove(Integer.valueOf(9));
-if(numbers.contains(Integer.valueOf(8))) {
-    System.out.println("yes");
+
 }
-if(!numbers.isEmpty()){
-    System.out.println("Not empty");
-}
-System.out.println(numbers);
-System.out.println(numbers.size());
-    }
-    public static void simple(){
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(12);
-        numbers.add(3);
-        numbers.add(7);
-        numbers.add(1);
-        Collections.sort(numbers);
-        System.out.println(numbers);
-        Collections.sort(numbers,Collections.reverseOrder());
-        System.out.println(numbers);
-        Collections.shuffle(numbers);
-        System.out.println(numbers);
-        int minVal= Collections.min(numbers);
-        System.out.println(minVal);
-        int maxVal=Collections.max(numbers);
-        System.out.println(maxVal);
-    }
-    public static void newArraylist(){
-        ArrayList<Integer>zeros = new ArrayList<>(Collections.nCopies(10,0));
-        System.out.println(zeros);
-    }
-}
+
 
 
